2f33a3ec49c4dd4345a5dafb07464493cbc6f890
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-01-20 15:25:29 +0000

    Tweaked tests for a gradual approach.

--- workspace-blockchain/test/Module-31/311-SimpleStorage.test.ts
+++ workspace-blockchain/test/Module-31/311-SimpleStorage.test.ts
@@ -1,27 +1,22 @@
 import { should } from 'chai';
 import { SimpleStorageInstance } from '../../types/truffle-contracts';
 
-
-const SimpleStorage = artifacts.require('./Module-31/SimpleStorage.sol') as Truffle.Contract<SimpleStorageInstance>;
 should();
 
+const SimpleStorage = artifacts.require('SimpleStorage') as Truffle.Contract<SimpleStorageInstance>;
+
 /** @test {SimpleStorage} contract */
-contract('SimpleStorage', (accounts) => {
-
-    let simpleStorage: SimpleStorageInstance;
-    const valueToStore = 42;
-
-
-    beforeEach(async () => {
-        simpleStorage = await SimpleStorage.new();
-    });
+contract('SimpleStorage', () => {
 
     /**
      * Test the two contract methods
      * @test {SimpleStorage#set} and {SimpleStorage#get}
      */
     it('Store and retrieve a value.', async () => {
-        await simpleStorage.set(valueToStore, { from: accounts[0] });
+        const simpleStorage: SimpleStorageInstance  = await SimpleStorage.new();
+
+        const valueToStore = 42;
+        await simpleStorage.set(valueToStore);
         const storedData = await simpleStorage.get();
 
         (storedData.toNumber()).should.be.equal(valueToStore);
@@ -32,6 +27,8 @@ contract('SimpleStorage', (accounts) => {
      * @test {SimpleStorage#get}
      */
     it('Retrieve a value before setting it.', async () => {
+        const simpleStorage: SimpleStorageInstance  = await SimpleStorage.new();
+
         const storedData = await simpleStorage.get();
 
         (storedData.toNumber()).should.be.equal(0);
