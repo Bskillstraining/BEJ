9c869492bf49582b60f9261f9237f8c58883fb8b
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-01-23 11:23:40 +0000

    Improved comments.

--- workspace-blockchain/contracts/Module-32/322-UserTable.sol
+++ workspace-blockchain/contracts/Module-32/322-UserTable.sol
@@ -8,12 +8,18 @@ pragma solidity ^0.5.10;
 contract UserTable {
     mapping (address => uint256) private table;
 
+    /**
+     * @dev Store a value for the user, without impact on the values stored by other users.
+     */
     function set(uint256 value)
         public
     {
         table[msg.sender] = value;
     }
 
+    /**
+     * @dev Retrieve the value stored by the user.
+     */
     function get()
         public
         view
