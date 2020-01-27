48687b0e9668de4ccdb865c0102ff152069ff216
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-01-27 16:25:42 +0000

    Introduce testing for internal methods.

--- workspace-blockchain/contracts/Module-33/334-MyERC20.sol
+++ workspace-blockchain/contracts/Module-33/334-MyERC20.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.5.10;
 
-import "@openzeppelin/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
@@ -8,23 +7,4 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @title MyERC20
  * @dev These contracts guide the user into building an ERC20 cryptocurrency.
  */
-contract MyERC20 is ERC20, Ownable {
-
-    constructor () // Verify this is needed
-        Ownable()
-        public
-    {}
-
-    function mint(address recipient, uint256 amount)
-        public
-        onlyOwner
-    {
-        _mint(recipient, amount);
-    }
-
-    function burn(uint256 amount)
-        public
-    {
-        _burn(msg.sender, amount);
-    }
-}
+contract MyERC20 is ERC20 {}
