4c6221f3a869f6336d95e5d21a6093ef970eee0b
albertocuestacanada <mail@albertocuesta.es>
2020-02-20 15:28:20 +0000

    Coded Classifieds contracts.

---
+++ workspace-blockchain/contracts/Section-5/51-Holdings.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.10;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract Holdings {
+
+    IERC20 currencyToken;
+
+    mapping (address => uint256) public holdings;
+
+    /// @dev Instantiate token contracts
+    constructor (address currencyTokenAddress) public {
+        currencyToken = IERC20(currencyTokenAddress);
+    }
+
+    /// @dev Store tokens in the contract.
+    /// @param amount How many tokens to store.
+    function store(uint256 amount) public {
+        currencyToken.transferFrom(msg.sender, address(this), amount);
+        holdings[msg.sender] = amount;
+    }
+
+    /// @dev Release stored tokens in the contract to a recipient.
+    /// @param recipient Who to send the tokens to.
+    function release(address recipient) public {
+        uint256 amount = holdings[msg.sender];
+        delete holdings[msg.sender];
+        currencyToken.transfer(recipient, amount);
+    }
+}
