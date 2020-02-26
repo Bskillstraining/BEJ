4548dda4bd3d8b4d8aff94b8c206e95d4b6580f4
albertocuestacanada <mail@albertocuesta.es>
2020-02-26 14:44:36 +0000

    Simplified EthVault

--- workspace-blockchain/contracts/Section-5/53-EthVault.sol
+++ workspace-blockchain/contracts/Section-5/53-EthVault.sol
@@ -7,14 +7,12 @@ contract EthVault {
 
     /// @dev Store ether in the contract.
     function store() public payable {
-        require(holdings[msg.sender] == 0, "Already holding.");
         holdings[msg.sender] = msg.value;
     }
 
     /// @dev Release ether stored in the contract to a recipient.
     /// @param recipient Who to send the tokens to.
     function release(address payable recipient) public {
-        require(holdings[msg.sender] != 0, "Not holding.");
         uint256 amount = holdings[msg.sender];
         delete holdings[msg.sender];
         recipient.transfer(amount);
