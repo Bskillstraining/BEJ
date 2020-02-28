65405c051da01e517c4e74849b2fbafa1d49ee87
albertocuestacanada <mail@albertocuesta.es>
2020-02-28 14:37:57 +0000

    Fixed bug and tests pass.

--- workspace-blockchain/contracts/Section-5/55-Issuance.sol
+++ workspace-blockchain/contracts/Section-5/55-Issuance.sol
@@ -5,59 +5,69 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
 /// @dev Implements a very simple issuance process for tokens
-contract IssuanceEth is Ownable, ERC20 {
+contract Issuance is Ownable, ERC20 {
     using SafeMath for uint256;
 
-    event InvestmentAdded(address investor, uint256 amount);
-    event InvestmentCancelled(address investor, uint256 amount);
+    event Invested(address investor, uint256 investment);
+    event Cancelled(address investor);
+    event Claimed(address investor, uint256 tokens);
+    event Withdrawn(uint256 proceedings);
+    event GoneLive();
 
     bool live;
     uint256 public price;
     mapping(address => uint256) public investments;
     ERC20 public currency;
 
-    constructor(uint256 issuePrice, address currencyAddress) public Ownable() ERC20() {
+    constructor(uint256 issuePrice, address currencyAddress)
+        public Ownable() ERC20()
+    {
         price = issuePrice;
         currency = ERC20(currencyAddress);
     }
 
+    /// @dev Invest into the issuance by sending ether to this function
+    function invest(uint256 investment) public {
+        require(live == false, "Cannot invest if live.");
+        require(
+            investment.mod(price) == 0,
+            "Cannot invest a fraction of the price."
+        );
+        currency.transferFrom(msg.sender, address(this), investment);
+        investments[msg.sender] = investments[msg.sender].add(investment);
+        emit Invested(msg.sender, investment);
+    }
+
     /// @dev Function for an investor to cancel his investment
-    function cancelInvestment() external {
+    function cancel() public {
         require (live == false, "Cannot cancel if live.");
-        uint256 amount = investments[msg.sender];
+        uint256 investment = investments[msg.sender];
         delete investments[msg.sender];
-        currency.transfer(msg.sender, amount);
-        emit InvestmentCancelled(msg.sender, amount);
+        currency.transfer(msg.sender, investment);
+        emit Cancelled(msg.sender);
     }
 
     /// @dev Use this function to claim your issuance tokens
-    function claim() external {
+    function claim() public {
         require(live == true, "Cannot claim until live.");
         uint256 investment = investments[msg.sender];
-        uint256 issuanceTokens = investment.div(price);
+        uint256 tokens = investment.div(price);
         delete investments[msg.sender];
-        mint(msg.sender, issuanceTokens);
-    }
-
-    /// @dev Invest into the issuance by sending ether to this function
-    function invest(uint256 amount) public {
-        require(live == false, "Cannot invest if live.");
-        require(
-            amount.mod(price) == 0,
-            "Fractional investments not allowed."
-        );
-        investments[msg.sender] = investments[msg.sender].add(amount);
-        emit InvestmentAdded(msg.sender, amount);
+        _mint(msg.sender, tokens);
+        emit Claimed(msg.sender, tokens);
     }
 
     /// @dev Function to move to the distributing phase
     function goLive() public onlyOwner {
         live = true;
+        emit GoneLive();
     }
 
     /// @dev Function to transfer all collected tokens to the wallet of the owner
     function withdraw() public onlyOwner {
         require(live == true, "Cannot withdraw until live.");
-        currency.transfer(owner(), currency.balanceOf(address(this)));
+        uint256 proceedings = currency.balanceOf(address(this));
+        currency.transfer(owner(), proceedings);
+        emit Withdrawn(proceedings);
     }
 }
--- workspace-blockchain/test/Section-5/55-Issuance.test.ts
+++ workspace-blockchain/test/Section-5/55-Issuance.test.ts
@@ -1,14 +1,10 @@
 import * as chai from 'chai';
-// tslint:disable-next-line:no-var-requires
-const { balance, BN, ether, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
-import { MyERC20Instance, IssuanceInstance } from '../../types/truffle-contracts';
-
+import { IssuanceInstance, MyERC20Instance } from '../../types/truffle-contracts';
 const Issuance = artifacts.require('Issuance') as Truffle.Contract<IssuanceInstance>;
 const MyERC20 = artifacts.require('MyERC20') as Truffle.Contract<MyERC20Instance>;
 
 // tslint:disable-next-line:no-var-requires
-// chai.use(require('chai-bn')(require('bn.js')));
-// chai.should();
+const { expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
 contract('Issuance', (accounts) => {
 
@@ -17,30 +13,119 @@ contract('Issuance', (accounts) => {
 
     const owner = accounts[0];
     const investor = accounts[1];
-    const supply = ether('1000');
-    const investorBalance = ether('100')
-    const price = ether('10');
+    const supply = '1000';
+    const investorBalance = '100';
+    const price = '10';
+    const investment = '10';
+    const tokensBought = '1';
 
     beforeEach(async () => {
         currency = await MyERC20.new(supply);
         issuance = await Issuance.new(price, currency.address);
-        currency.transfer(investor, investorBalance, { from: owner});
+        currency.transfer(investor, investorBalance, { from: owner });
+        await currency.approve(issuance.address, investment, { from: investor });
     });
 
-    it('cannot claim if not live', async () => {});
-    it('cannot withdraw if not live', async () => {});
-    it('cannot invest a fraction of the price', async () => {});
-    it('can invest', async () => {});
-    describe('after investing', () => {
-        it('can cancel investment', async () => {});
+    it('cannot claim if not live', async () => {
+        await expectRevert(
+            issuance.claim({ from: investor }),
+            'Cannot claim until live.',
+        );
     });
-    it('can go live', async () => {});
+
+    it('cannot withdraw if not live', async () => {
+        await expectRevert(
+            issuance.withdraw({ from: owner }),
+            'Cannot withdraw until live.',
+        );
+    });
+
+    it('cannot invest a fraction of the price', async () => {
+        await expectRevert(
+            issuance.invest(1, { from: investor }),
+            'Cannot invest a fraction of the price.',
+        );
+    });
+
+    it('can invest', async () => {
+        expectEvent(
+            await issuance.invest(investment, { from: investor }),
+            'Invested',
+            {
+                investment: investment,
+                investor: investor,
+            },
+        );
+    });
+
+    describe('after investing', () => {
+        beforeEach(async () => {
+            await issuance.invest(investment, { from: investor });
+        });
+
+        it('can cancel investment', async () => {
+            expectEvent(
+                await issuance.cancel({ from: investor }),
+                'Cancelled',
+                {
+                    investor: investor,
+                },
+            );
+        });
+    });
+
+    it('can go live', async () => {
+        expectEvent(
+            await issuance.goLive({ from: owner }),
+            'GoneLive',
+            {},
+        );
+    });
+
     describe('after going live', () => {
-        it('cannot invest if live', async () => {});
-        it('cannot cancel investment if live', async () => {});
-        describe('after going live and having investments', () => {
-            it('can claim', async () => {});
-            it('can withdraw', async () => {});
+        beforeEach(async () => {
+            await issuance.goLive({ from: owner });
+        });
+
+        it('cannot invest if live', async () => {
+            await expectRevert(
+                issuance.invest(investment, { from: investor }),
+                'Cannot invest if live.',
+            );
+        });
+
+        it('cannot cancel investment if live', async () => {
+            await expectRevert(
+                issuance.cancel({ from: investor }),
+                'Cannot cancel if live.',
+            );
+        });
+    });
+
+    describe('after going live and having investments', () => {
+        beforeEach(async () => {
+            await issuance.invest(investment, { from: investor });
+            await issuance.goLive({ from: owner });
+        });
+
+        it('can claim', async () => {
+            expectEvent(
+                await issuance.claim({ from: investor }),
+                'Claimed',
+                {
+                    investor: investor,
+                    tokens: tokensBought,
+                },
+            );
+        });
+        it('can withdraw', async () => {
+            expectEvent(
+                await issuance.withdraw({ from: owner }),
+                'Withdrawn',
+                {
+                    proceedings: investment,
+                },
+            );
         });
     });
 });
