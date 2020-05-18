0aedbd08be66c5702014670335bffc7bcf94d1b2
Alberto Cuesta Cañada <mail@albertocuesta.es>
2020-05-18 06:48:26 +0100

    SImplified 2_deploy_contracts.js

--- workspace-blockchain/migrations/2_deploy_contracts.js
+++ workspace-blockchain/migrations/2_deploy_contracts.js
@@ -1,6 +1,5 @@
-const SimpleStorage = artifacts.require('./SimpleStorage.sol');
+const SimpleStorage = artifacts.require('SimpleStorage.sol');
 
 module.exports = async (deployer, network, accounts) => {
     await deployer.deploy(SimpleStorage);
-    await deployer.ens.setAddress('simplestorage.eth', SimpleStorage.address, { from: accounts[0] })
 }
